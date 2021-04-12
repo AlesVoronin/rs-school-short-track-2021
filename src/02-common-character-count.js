4e3bcdae75d1b60d8f3adb8421563aa5af788c69
AlesVoronin <alesworonin@gmail.com>
2021-04-13 00:17:22 +0300

    feat: complete 3 task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -10,30 +9,29 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(s1, s2) {
-  let temp = [''];
-  s1 = s1.split('');
-  s2 = s2.split('');
+function getCommonCharacterCount(/* s1, s2 */) {
+  // let temp = [''];
+  // s1 = s1.split('');
+  // s2 = s2.split('');
+  //
+  // for (let i = 0; i < s1.length; i++){
+  //   for (let j = 0; j < s2.length; j++){
+  //     if (s1[i] == s2[j]){
+  //       for (let k = 0; k <= temp.length; k++){
+  //         if (s2[j] == temp[k]){
+  //           break;
+  //         }
+  //         else {
+  //           temp = temp.push(s2[j]);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  // temp = temp.join('');
+  // return temp;
 
-  for (let i = 0; i < s1.length; i++){
-    for (let j = 0; j < s2.length; j++){
-      if (s1[i] == s2[j]){
-        for (let k = 0; k <= temp.length; k++){
-          if (s2[j] == temp[k]){
-            break;
-          }
-          else {
-            temp = temp.push(s2[j]);
-          }
-        }
-      }
-    }
-  }
-  temp = temp.join('');
-  return temp;
-
-
-  // throw new Error('Not implemented');
+  throw new Error('Not implemented');
 }
 
-//module.exports = getCommonCharacterCount;
+module.exports = getCommonCharacterCount;
